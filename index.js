2e157921021f10e704cf0c28b909577a31132ce6
Juanjo Villar <juanjovillar@live.com>
2018-10-05 18:11:19 +0200

    Basic import

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const userName = "Juanjo";
-const messageToDisplay = `Hello ${userName}`;
+import { getAvg } from "./service";
 
+const scores = [1, 5, 10, 20];
+const averageScore = getAvg(scores);
+const messageToDisplay = `average score ${averageScore}`;
 
 document.write(messageToDisplay);
