e44ee01adea1d0a71b005c6e047b450648aea50f
Juanjo Villar <juanjovillar@live.com>
2018-10-05 19:58:40 +0200

    Default import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getAvg } from "./service";
+import getAvg from "./service";
 
 const scores = [1, 5, 10, 20];
 const averageScore = getAvg(scores);
--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-export function getAvg(scores){
+export default function getAvg(scores){
     return getTotalScore(scores);
 }
 
